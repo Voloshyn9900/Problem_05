7c5ef3d11f908d9dfe94a7c443ee206951401838
Yurii Voloshyn <voloshyn9900@gmail.com>
2022-11-23 22:04:23 +0100

    commit2

--- proj/js/slider.js
+++ proj/js/slider.js
@@ -23,6 +23,24 @@ document.querySelector('.button__slide--3').addEventListener('click', function (
 });
 
 
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
+
+
+
+
 
 // let offset = 0;
 // const sliderLine = document.querySelector('.slider__line');
